637b55842e4d14545825d2aecde5fc26fadea07f
Matúš Hubinský <matus.hubinsky@scot.sk>
2023-04-26 15:17:38 +0200

    style(main): added doxy comments

--- main.c
+++ main.c
@@ -24,49 +24,59 @@ int tz;	// the maximum waiting time that customers waits
 int tu;	// the maximum length of the official's break time
 int f;	// the maximum time in miliseconds
 
-FILE* output;
+FILE* output; // output file
 
-sem_t* xhubin04_semaphore_letter = NULL;
-sem_t* xhubin04_semaphore_package = NULL;
-sem_t* xhubin04_semaphore_money = NULL;
-sem_t* xhubin04_semaphore_mutex = NULL;
-sem_t* xhubin04_semaphore_write = NULL;
+sem_t* xhubin04_semaphore_letter = NULL;  	// letter queue semaphore
+sem_t* xhubin04_semaphore_package = NULL; 	// package queue semaphore
+sem_t* xhubin04_semaphore_money = NULL;		// money queue semaphore
+sem_t* xhubin04_semaphore_mutex = NULL;		// mutex semaphore
+sem_t* xhubin04_semaphore_write = NULL;		// semaphore for writing to output file
 
-int* queue_letter = NULL;
-int* queue_package = NULL;
-int* queue_money = NULL;
-int* line_number = NULL;
-bool* post_office = NULL;
+int* queue_letter = NULL;	// number of customers in letter queue
+int* queue_package = NULL;	// number of customers in package queue
+int* queue_money = NULL;	// number of customers in money queue
+int* line_number = NULL;	// number of current line
+bool* post_office = NULL;	// post office status (true - open, false - closed)
 
 
+/*
+ * @brief open all semaphores 
+ * @param
+ * @returns
+*/
 static void semaphores_open_all() {
-	if ((xhubin04_semaphore_letter = sem_open("xhubin04_semaphore_letter", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED){
+	if ((xhubin04_semaphore_letter = sem_open("xhubin04_semaphore_letter", O_CREAT | O_EXCL, 0664, 0)) == SEM_FAILED){
         fprintf(stderr, "sem_open: xhubin04_semaphore_letter\n");
         exit(1);
     }
 
-	if ((xhubin04_semaphore_package = sem_open("/xhubin04_semaphore_package", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED){
+	if ((xhubin04_semaphore_package = sem_open("/xhubin04_semaphore_package", O_CREAT | O_EXCL, 0664, 0)) == SEM_FAILED){
         fprintf(stderr, "sem_open: xhubin04_semaphore_package\n");
         exit(1);
     }
 
-	if ((xhubin04_semaphore_money = sem_open("/xhubin04_semaphore_money", O_CREAT | O_EXCL, 0666, 0)) == SEM_FAILED){
+	if ((xhubin04_semaphore_money = sem_open("/xhubin04_semaphore_money", O_CREAT | O_EXCL, 0664, 0)) == SEM_FAILED){
         fprintf(stderr, "sem_open: xhubin04_semaphore_money\n");
         exit(1);
     }
 
-	if ((xhubin04_semaphore_mutex = sem_open("/xhubin04_semaphore_mutex", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED){
+	if ((xhubin04_semaphore_mutex = sem_open("/xhubin04_semaphore_mutex", O_CREAT | O_EXCL, 0664, 1)) == SEM_FAILED){
         fprintf(stderr, "sem_open: xhubin04_semaphore_mutex\n");
         exit(1);
     }
 
-	if ((xhubin04_semaphore_write = sem_open("/xhubin04_semaphore_write", O_CREAT | O_EXCL, 0666, 1)) == SEM_FAILED){
+	if ((xhubin04_semaphore_write = sem_open("/xhubin04_semaphore_write", O_CREAT | O_EXCL, 0664, 1)) == SEM_FAILED){
         fprintf(stderr, "sem_open: xhubin04_semaphore_write\n");
         exit(1);
     }
 }
 
 
+/*
+ * @brief close all semaphores 
+ * @param
+ * @return
+*/
 static void semaphores_close_all() {
 	if (sem_close(xhubin04_semaphore_letter) == -1) {
         fprintf(stderr, "sem_open: xhubin04_semaphore_letter\n");
@@ -95,6 +105,11 @@ static void semaphores_close_all() {
 }
 
 
+/*
+ * @brief unlink all semaphores 
+ * @param
+ * @return
+*/
 static void semaphores_unlink_all() {
 	sem_unlink("xhubin04_semaphore_letter");
 	sem_unlink("xhubin04_semaphore_package");
@@ -104,6 +119,11 @@ static void semaphores_unlink_all() {
 }
 
 
+/*
+ * @brief initialize shared memory 
+ * @param
+ * @return
+*/
 static void mmap_init() {
 	queue_letter = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
 	queue_package = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -113,6 +133,11 @@ static void mmap_init() {
 }
 
 
+/*
+ * @brief unlink shared memory 
+ * @param
+ * @return
+*/
 static void mmap_unlink() {
 	munmap(queue_letter, sizeof(int));
 	munmap(queue_package, sizeof(int));
@@ -122,6 +147,12 @@ static void mmap_unlink() {
 }
 
 
+
+/*
+ * @brief close semaphore for writing, write message and open semaphore
+ * @param format string, same as printf
+ * @return
+*/
 void write_to_file(char *format, ...) {
 	sem_wait(xhubin04_semaphore_write);
 
@@ -136,6 +167,24 @@ void write_to_file(char *format, ...) {
 }
 
 
+/*
+ * @brief process customer 
+ * @param id of customer
+ * @return
+*/
+bool customers_in_queue() {
+	if ((*queue_letter) || (*queue_package) || (*queue_money)) {
+		return true;
+	}
+	return false; 
+}
+
+
+/*
+ * @brief process customer 
+ * @param id customer's id
+ * @return
+*/
 void customer(int id) {
 	srand(time(0) ^ getpid());
 	
@@ -164,16 +213,11 @@ void customer(int id) {
 }
 
 
-bool customers_in_queue() {
-	// fprintf(stderr, "%d %d %d\n", (*queue_letter), (*queue_package), (*queue_money));
-	// fflush(stderr);
-	if ((*queue_letter) || (*queue_package) || (*queue_money)) {
-		return true;
-	}
-	return false; 
-}
-
-
+/*
+ * @brief process official 
+ * @param id official's id
+ * @return
+*/
 void official(int id) {
 	srand(time(0) ^ getpid());
 	write_to_file("U %d: started\n", id);
@@ -228,25 +272,37 @@ void official(int id) {
 }
 
 
-bool is_number(char *ptr) {
-	long long num;
-	num = strtol(ptr, &ptr, 10);
-	if (*ptr == '\0') {
-		num++;
+/*
+ * @brief check if string is number 
+ * @param string pointer to string
+ * @return true when the string was a number, false otherwise
+*/
+bool is_number(char *string) {
+	int number;
+	number = strtol(string, &string, 10);
+	if (*string == '\0') {
+		number++;
 		return true;
 	}
 	else {
-		num++;
+		number++;
 		return false;
 	}
 }
 
 
-int main(int argc, char *argv[]) {
+/*
+ * @brief check all argumetns, assign values to variables, check if arguments are correct
+ * print error message and exit with code 1 if arguments are not correct
+ * @param argc nubmer of arguments
+ * @param argv string array of arguments
+ * @return
+*/
+void process_arguments(int argc, char *argv[]) {
 	// check number of arguments
 	if (argc != ARG_NUM) {
 		fprintf(stderr, "Error: Wrong number of argments!\n");
-		return 1;
+		exit(1);
 	}
 
 	// assing arguments values
@@ -259,19 +315,26 @@ int main(int argc, char *argv[]) {
 	// check if arguments are correct, they must be bigger than 0
 	if (nz < -1 || nu < 1) {
 		fprintf(stderr, "Error: Wrong number of people!\n");
-		return 1;
+		exit(1);
 	}
 
+	// check if arguments are numbers and not strings
 	if (!is_number(argv[1]) || !is_number(argv[2]) || !is_number(argv[3]) || !is_number(argv[4]) || !is_number(argv[5])) {
 		fprintf(stderr, "Error: Arguments are not numbers!\n");
-		return 1;	
+		exit(1);	
 	}
 
 	// check if waiting times are correct, it must be bigger than 0 and lower than 1000
 	if ((tz < 0 || tz > 10000) || (tu < 0 || tu > 100) || (f < 0 || f > 1000)) {
 		fprintf(stderr, "Error: Wrong waiting time!\n");
-		return 1;	
+		exit(1);	
 	}
+}
+
+
+int main(int argc, char *argv[]) {
+	// process all arguments and assign values to variables
+	process_arguments(argc, argv);
 
 	output = fopen("proj2.out", "w");
 	if (output == NULL) {
@@ -282,39 +345,43 @@ int main(int argc, char *argv[]) {
 	// open all semaphores
 	semaphores_open_all();
 	
-	// s
+	// map all shared variables
 	mmap_init();
 	
+	// initialize all shared variables
 	memory_lock(*line_number = 1);
 	memory_lock(*post_office = true);
 
+	// fork all customers
 	for (int i = 0; i < nz; i++) {
 		pid_t customer_pid = fork();
 		if (customer_pid == 0) {
 			customer(i + 1);
 			return 0;
 		} else if (customer_pid == -1) {
-			fprintf(stderr, "Error: fork\n");
+			fprintf(stderr, "Error: can't fork customer\n");
 			return 1;
 		}
 	}
 
+	// fork all officials
 	for (int i = 0; i < nu; i++) {
 		pid_t official_pid = fork();
 		if (official_pid == 0) {
 			official(i + 1);
 			return 0;
 		} else if (official_pid == -1) {
-			fprintf(stderr, "Error: fork\n");
+			fprintf(stderr, "Error: can't fork official\n");
 			return 1;
 		}
 	}
 
+	// go for sleep for random nubmer in range <f/2, f>, then close the post office
 	usleep(((f/2) + (rand() % (f/2 + 1))) * 1000);
 	write_to_file("closing\n");
 	memory_lock(*post_office = false);
 	
-	// close all children
+	// wait for all children to die
 	while(wait(NULL) > 0);
 	
 	// close all semaphores
